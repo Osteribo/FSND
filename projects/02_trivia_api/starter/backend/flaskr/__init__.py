1984746337649e404673da92f915eec926302c31
ALO <36780370+Osteribo@users.noreply.github.com>
2020-01-03 16:44:54 -0800

    Update __init__.py

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -16,17 +16,31 @@ def create_app(test_config=None):
   '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
-
+  CORS(app)
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
 
   '''
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
-
+  @app.route('/catagories', methods = ['GET'])
+  def get_catagories():
+    categories = Category.query.all()
+    formatted_categories= [category.format() for category in categories]
+    return jsonify({
+            'success':True,
+            'books': formatted_categories,
+            'total_books': len(formatted_categories)
+        })
 
   '''
   @TODO: 
@@ -39,7 +53,20 @@ def create_app(test_config=None):
   you should see questions and categories generated,
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions. 
-  '''
+      '''
+
+  @app.route('/questions', methods = ['GET'])
+  def get_questions():
+    page = request.args.get('page', 1, type=int)
+    start = (page -1) + 10
+    end = start +10
+    questions = Question.query.all()
+    formatted_questions= [question.format() for question in questions]
+    return jsonify({
+            'success':True,
+            'questions': formatted_questions,
+            'total_questions': len(formatted_questions)
+        })
 
   '''
   @TODO: 
