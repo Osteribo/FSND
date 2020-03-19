27da7af460a0873ee86f671112fa41de7f6dd781
ALO <36780370+Osteribo@users.noreply.github.com>
2020-03-19 16:09:49 -0700

    Chore: new various files
    pull in all the required files and parts so i can start to add in logic

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -2,11 +2,23 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import setup_db
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   CORS(app)
+
+  @app.route('/')
+  def get_greeting():
+      excited = os.environ['EXCITED']
+      greeting = "Hello" 
+      if excited == 'true': greeting = greeting + "!!!!!"
+      return greeting
+
+  @app.route('/coolkids')
+  def be_cool():
+      return "Be cool, man, be coooool! You're almost a FSND grad!"
 
   return app
 
