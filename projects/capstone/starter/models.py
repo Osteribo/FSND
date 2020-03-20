e202d7875e03cb4a821b63d2340f2555231aa8c7
ALO <36780370+Osteribo@users.noreply.github.com>
2020-03-19 21:29:17 -0700

    Fix: local working app
    finished the app for local use. Authentication and RBAC setup. Added in information for heroku deploymnet

--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -3,10 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 import os
 
-database_path= os.environ['DATABASE_URL']
+# database_path= os.environ['DATABASE_URL']
 
-# database_name = "fsdn"
-# database_path = "postgres://{}:{}@{}/{}".format('alo', '1234', 'localhost:5432', database_name)
+database_name = "fsnd_capstone"
+database_path = "postgres://{}:{}@{}/{}".format('alo', '1234', 'localhost:5432', database_name)
 
 db = SQLAlchemy()
 
@@ -23,6 +23,14 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
+
+def db_drop_and_create_all():
+    '''drops the database tables and starts fresh
+    can be used to initialize a clean database
+    '''
+    db.drop_all()
+    db.create_all()
+    db_init_records()
 '''
 Donor
 
@@ -33,23 +41,118 @@ class Donor(db.Model):
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    date_of_birth = Column(Date)
-    blood_type = Column(String)
-    products = Column(String)
-    num_products = Column(Integer)
+    donation = Column(Integer)
+    
+    '''
+    long()
+        long form representation of the Drink model
+    '''
+    def long(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'donation' : self.donation,
+           
+        }
+    '''
+    insert()
+        inserts a new model into a database
+        the model must have a unique name
+        the model must have a unique id or null id
+        EXAMPLE
+            drink = Drink(title=req_title, recipe=req_recipe)
+            drink.insert()
+    '''
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+    delete()
+        deletes a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            drink = Drink(title=req_title, recipe=req_recipe)
+            drink.delete()
+    '''
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    '''
+    update()
+        updates a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            drink = Drink.query.filter(Drink.id == id).one_or_none()
+            drink.title = 'Black Coffee'
+            drink.update()
+    '''
+    def update(self):
+        db.session.commit()
+
+    def __repr__(self):
+        return json.dumps(self.long())
+
+    
 
 '''
 Product
-#later implementation
-
-class Product(db.Model):
-    __tablename__="Product"
+ later implementation
+'''
+class Program(db.Model):
+    __tablename__="Program"
 
     id = Column(Integer, primary_key=True)
-    cell_type = Column(String)
-    date_of_processing = Column(Date)
-    donor = 
+    division = Column(String)
+    director = Column(String)
+
+    def long(self):
+        return {
+            'id': self.id,
+            'name': self.division,
+            'donation' : self.director,
+           
+        }
 
 
-'''
+    '''
+    insert()
+        inserts a new model into a database
+        the model must have a unique name
+        the model must have a unique id or null id
+        EXAMPLE
+            drink = Drink(title=req_title, recipe=req_recipe)
+            drink.insert()
+    '''
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+    delete()
+        deletes a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            drink = Drink(title=req_title, recipe=req_recipe)
+            drink.delete()
+    '''
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    '''
+    update()
+        updates a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            drink = Drink.query.filter(Drink.id == id).one_or_none()
+            drink.title = 'Black Coffee'
+            drink.update()
+    '''
+    def update(self):
+        db.session.commit()
+
+    def __repr__(self):
+        return json.dumps(self.long())
 
